7751b13288defe24dd4fac7a087ecb34d358d59b
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-07 13:32:06 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "https://overback.onrender.com/api";
+export const API_BASE = "https://completed-backend.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE,
