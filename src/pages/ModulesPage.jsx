5cf8b989931135c29a1a9c58ae4c96e800b04d91
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-07 22:37:03 +0530

    Update ModulesPage.jsx

--- src/pages/ModulesPage.jsx
+++ src/pages/ModulesPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import API from "../api/adminApi";
+import API from "../api/Api";
 
 export default function Modules() {
   const [modules, setModules] = useState([]);
