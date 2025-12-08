cc13d64e17b4b8cb5da74a3878b4d586d76862ff
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-08 17:39:14 +0530

    Update ProductsPage.jsx

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getProduct } from "../api";
+import { getProduct } from "../api/api";
 
 
 function Product() {
