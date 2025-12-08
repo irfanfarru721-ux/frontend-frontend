b77c1221362d85290f01285cea4e619bd39fde50
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-08 08:25:48 +0530

    Update CategoryProductsPage.jsx

--- src/pages/CategoryProductsPage.jsx
+++ src/pages/CategoryProductsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "../api";
+import axios from "../api/api";
 
 export default function CategoryProducts() {
   const { categoryId } = useParams();
