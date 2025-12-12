977296865d1d68bc20c4d274863565a832e95154
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-12 11:52:16 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../api";
+import API from "../api/api";
 
 export default function Login() {
   const [email, setEmail] = useState("");
